2dfa20d26c8970b8ff0d82cb0a5817f9a3e61945
Lê Nhật Khanh <pankenzak@gmail.com>
2026-03-07 00:17:14 +0700

    Add files via upload

---
+++ Algorithms/Sorts/Insertion Sort.py
@@ -0,0 +1,14 @@
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+
+def main():
+    arr = [3,5,8,9,1,2,0]
+    insertionSort(arr)
+    print(arr)
+
+main()
